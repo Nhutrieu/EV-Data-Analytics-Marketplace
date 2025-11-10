d5579b40fb402acc248173d00019a01954788d19
VanTruong2509 <truongnv2561@ut.edu.vn>
2025-11-10 12:11:34 +0700

    update

--- auth-service/src/Config/Database.php
+++ auth-service/src/Config/Database.php
@@ -5,11 +5,20 @@ use mysqli;
 
 class Database {
     public static function connect() {
-        $conn = new mysqli("localhost", "root", "", "ev_data_analytics_marketplace");
+        $conn = new mysqli(
+            "db",                       // Docker service name
+            "root",                     // MySQL user
+            "rootpassword",             // MySQL password
+            "ev_data_analytics_marketplace" // Database name
+        );
 
         if ($conn->connect_error) {
-            die(json_encode(["status" => "error", "message" => "DB connect failed"]));
+            die(json_encode([
+                "status" => "error",
+                "message" => "DB connect failed: " . $conn->connect_error
+            ]));
         }
+
         return $conn;
     }
 }
